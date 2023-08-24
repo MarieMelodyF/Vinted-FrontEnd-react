e7e5273e945911d264e13189d4437f33aa218d37
Marie Mélody F <mariemelo@mbp-de-marie.lan>
2023-08-24 15:58:52 +0200

    form-signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -8,98 +8,128 @@ const Signup = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [newsletter, setNewsletter] = useState("");
+  const [newsletter, setNewsletter] = useState(false);
   const [showResults, setShowResults] = useState(false);
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
+    console.log(event.target.value);
   };
-
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
+    console.log(event.target.value);
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
+    console.log(event.target.value);
   };
   const handleNewslettersChange = (event) => {
-    setNewsletter(event.target.value);
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setShowResults(true);
+    setNewsletter(!newsletter);
+    console.log(!newsletter);
   };
 
-  useEffect(() => {
-    console.log(data);
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://lereacteur-vinted-api.herokuapp.com/user/signup"
-        );
-        console.log("response.data =>", response.data);
-        setData(response.data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      const data = {
+        username: username,
+        email: email,
+        password: password,
+        newsletter: newsletter,
+      };
+      const response = await axios.post(
+        "https://lereacteur-vinted-api.herokuapp.com/user/signup",
+        {
+          username: username,
+          password: password,
+          email: email,
+          newsletter: newsletter,
+        }
+      );
+      console.log("response.data =>", response.data);
 
-    fetchData();
-  }, []);
+      setShowResults(true);
+      console.log("Submit ==> ", data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //   useEffect(() => {
+  //     // console.log(data);
+  //     const fetchData = async () => {
+  //       try {
+  //         const response = await axios.get(
+  //           "https://lereacteur-vinted-api.herokuapp.com/user/signup",
+  //           { username: username, password: password, email: email }
+  //         );
+  //         console.log("response.data =>", response.data);
+  //         setData(response.data);
+  //         setIsLoading(false);
+  //       } catch (error) {
+  //         console.log(error.message);
+  //       }
+  //     };
+
+  //     fetchData();
+  //   }, []);
   return (
-    <main className="container">
-      <div className="signup">
-        <h1>S'inscrire</h1>
-        <form id="contactForm" onSubmit={handleSubmit}>
-          <input
-            onChange={handleUsernameChange}
-            type="text"
-            name="Nom d'utilisateur"
-            id="Nom d'utilisateur"
-            value={username}
-            placeholder="Nom d'utilisateur"
-          />
-          <br />
-          <input
-            onChange={handleEmailChange}
-            type="email"
-            name="email"
-            value={email}
-            placeholder="john.doe@gmail.com"
-          />
-          <br />
-          <input
-            onChange={handlePasswordChange}
-            type="password"
-            name="password"
-            value={password}
-            placeholder="Votre mot de passe"
-          />
-          <br />
-          <div className="checkbox">
+    username,
+    (
+      <main className="container">
+        <div className="signup">
+          <h1>S'inscrire</h1>
+          <form id="contactForm" onSubmit={handleSubmit}>
             <input
-              onChange={handleNewslettersChange}
-              className="check-input"
-              type="checkbox"
-              value={newsletter}
-              name="newsletter"
+              onChange={handleUsernameChange}
+              type="text"
+              name="username"
+              id="Nom d'utilisateur"
+              state={username}
+              placeholder="Nom d'utilisateur"
             />
-            <span>S'incre à notre newsletters</span>
-          </div>
-          <div className="politique">
-            <p>
-              en m'inscrivant je confirme avoir lu et accepté les Termes &
-              Conditions <br />
-              et Politique de Confidentialité de Vinted. Je confirme avoir au
-              moins 18 ans.{" "}
-            </p>
-          </div>
-          <br />
-          <button type="submit">S'inscrire</button>
-        </form>
-      </div>
-    </main>
+            <br />
+            <input
+              onChange={handleEmailChange}
+              type="email"
+              name="email"
+              state={email}
+              placeholder="john.doe@gmail.com"
+            />
+            <br />
+            <input
+              onChange={handlePasswordChange}
+              type="password"
+              name="password"
+              state={password}
+              placeholder="Votre mot de passe"
+            />
+            <br />
+            <div className="checkbox">
+              <input
+                onChange={handleNewslettersChange}
+                className="check-input"
+                type="checkbox"
+                state={newsletter}
+                name="newsletter"
+              />
+              <span>S'incre à notre newsletters</span>
+            </div>
+            <div className="politique">
+              <p>
+                en m'inscrivant je confirme avoir lu et accepté les Termes &
+                Conditions <br />
+                et Politique de Confidentialité de Vinted. Je confirme avoir au
+                moins 18 ans.{" "}
+              </p>
+            </div>
+            <br />
+            <button type="submit">S'inscrire</button>
+          </form>
+        </div>
+      </main>
+    )
   );
 };
 
